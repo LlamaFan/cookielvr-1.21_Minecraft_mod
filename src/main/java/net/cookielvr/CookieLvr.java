35cd5cf914f4367bb8d9841552ec2d375b3f279a
LlamaFan <nothingemailneu@gmail.com>
2024-10-16 17:06:01 +0200

    Initial commit

---
+++ src/main/java/net/cookielvr/CookieLvr.java
@@ -0,0 +1,15 @@
+package net.cookielvr;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CookieLvr implements ModInitializer {
+	public static final String MOD_ID = "cookielvr";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/cookielvr/CookieLvrDataGenerator.java
@@ -0,0 +1,11 @@
+package net.cookielvr;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CookieLvrDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
