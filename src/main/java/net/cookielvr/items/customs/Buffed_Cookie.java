7ced951fee2ed1a941a6e12836c8f82c6289eb31
LlamaFan <nothingemailneu@gmail.com>
2024-10-26 12:13:18 +0200

    New things added

---
+++ src/main/java/net/cookielvr/items/customs/Buffed_Cookie.java
@@ -0,0 +1,9 @@
+package net.cookielvr.items.customs;
+
+import net.minecraft.item.Item;
+
+public class Buffed_Cookie extends Item {
+    public Buffed_Cookie(Settings settings) {
+        super(settings);
+    }
+}
---
+++ src/main/java/net/cookielvr/items/customs/Cookie_Staff.java
@@ -0,0 +1,19 @@
+package net.cookielvr.items.customs;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.StackReference;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.util.ClickType;
+
+public class Cookie_Staff extends Item {
+    public Cookie_Staff(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public boolean onClicked(ItemStack stack, ItemStack otherStack, Slot slot, ClickType clickType, PlayerEntity player, StackReference cursorStackReference) {
+        return super.onClicked(stack, otherStack, slot, clickType, player, cursorStackReference);
+    }
+}
